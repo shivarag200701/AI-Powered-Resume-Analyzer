ba624cba8a08a71b4167e0976113d9af30495093
shivarag200701 <shivaraghav200701@gmail.com>
2025-07-21 18:11:33 -0500

    Implemented wipe functionality

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "gpt-4o-mini" }
+      { model: "claude-3-7-sonnet" }
     ) as Promise<AIResponse | undefined>;
   };
 
